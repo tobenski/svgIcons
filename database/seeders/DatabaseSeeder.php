be7b00dd815344037b86d71194ae5f04204e052f
Knud Rishøj <tirdyr@tobenski.dk>
2020-11-08 23:33:33 +0100

    added default user and removed registration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('Password'),
+        ]);
     }
 }
